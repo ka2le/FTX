3e68cecdf29104b89f296d54f50df369fe49e9c2
kalle.kindvall <kalle.kindvall@dacsa.se>
2023-10-06 18:30:30 +0200

    New icons and app Name and code

--- src/App.js
+++ src/App.js
@@ -8,79 +8,78 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
+  Divider,
+  IconButton,
   DialogTitle,
 } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
-const App = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
-  const [showOnlySelected, setShowOnlySelected] = useState(false);
-  const [openPopup, setOpenPopup] = useState(false);
-  
-  const toggleShowOnlySelected = () => {
-    // Placeholder function for toggling show only selected
-  };
+function App() {
+  const [currentTab, setCurrentTab] = useState(0);
+  const [showSelected, setShowSelected] = useState(false);
+  const [tableHeight, setTableHeight] = useState(300); // Initial height in pixels
 
-  const handleTabChange = (event, newValue) => {
-    setSelectedTab(newValue);
-  };
-
-  const handleOpenPopup = () => {
-    setOpenPopup(true);
-  };
-
-  const handleClosePopup = () => {
-    setOpenPopup(false);
-  };
-
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Name', width: 130 },
+  // Sample Data for DataGrids
+  const combos = [
+    { id: 1, combo: 'Avocado Toast', ingredients: ['Avocado', 'Bread'] },
+    // ... more combos
+  ];
+  const ingredients = [
+    { id: 1, ingredient: 'Avocado', utility: 'High' },
+    // ... more ingredients
   ];
 
-  const rows = [
-    { id: 1, name: 'Combo 1' },
-    { id: 2, name: 'Combo 2' },
-    // Add more dummy data here
-  ];
+  const handleDividerDrag = (e) => {
+    // Logic for draggable divider
+  };
 
   return (
     <div>
-      <AppBar position="static">
-        <Tabs value={selectedTab} onChange={handleTabChange}>
-          <Tab label="Combos" />
-          <Tab label="Ingredients" />
+      {/* Fixed Section */}
+      <AppBar position="sticky">
+        {/* Selection-Tabs */}
+        <Tabs value={currentTab} onChange={(e, newVal) => setCurrentTab(newVal)}>
+          <Tab label="Tab 1" />
+          <Tab label="Tab 2" />
+          {/* Add Tab Button */}
+          <Button color="inherit">Add Tab</Button>
         </Tabs>
+        {/* Selection-Field and "+" Button */}
+        <div>
+          <span>Selected Ingredients Here</span>
+          <Button color="inherit">+</Button>
+        </div>
+        {/* Toggle Button */}
+        <Button color="inherit" onClick={() => setShowSelected(!showSelected)}>
+          {showSelected ? 'Show All' : 'Show Only Selected'}
+        </Button>
       </AppBar>
-      
-      <Button onClick={handleOpenPopup}>+</Button>
-      <Button onClick={toggleShowOnlySelected}>Show Only Selected</Button>
-      
-      {selectedTab === 0 && (
-        <Paper style={{ height: 400, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} pageSize={5} />
-        </Paper>
-      )}
 
-      {selectedTab === 1 && (
-        <Paper style={{ height: 400, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} pageSize={5} />
+      {/* Scrollable Section */}
+      <div style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 64px)' }}>
+        {/* Combos Table */}
+        <Paper style={{ height: tableHeight, overflow: 'auto' }}>
+          <DataGrid rows={combos} columns={[{ field: 'combo', headerName: 'Combo' }, { field: 'ingredients', headerName: 'Ingredients' }]} />
         </Paper>
-      )}
-
-      <Dialog open={openPopup} onClose={handleClosePopup}>
-        <DialogTitle>Select Ingredients</DialogTitle>
-        <DialogContent>
-          {/* Add ingredient selection UI here */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClosePopup}>Close</Button>
-        </DialogActions>
-      </Dialog>
+        {/* Draggable Divider */}
+        <Divider orientation="horizontal" />
+        <div>
+          <IconButton onClick={() => setTableHeight(tableHeight - 20)}>
+            <ArrowUpwardIcon />
+          </IconButton>
+          <IconButton onClick={() => setTableHeight(tableHeight + 20)}>
+            <ArrowDownwardIcon />
+          </IconButton>
+        </div>
+        {/* Ingredient Utility Table */}
+        <Paper style={{ flex: 1, height: `calc(100vh - ${tableHeight + 64}px)`, overflow: 'auto' }}>
+          <DataGrid rows={ingredients} columns={[{ field: 'ingredient', headerName: 'Ingredient' }, { field: 'utility', headerName: 'Utility' }]} />
+        </Paper>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
