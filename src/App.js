4b07971319d37cfe6e74e86146426aa90dbd7f32
kalle.kindvall <kalle.kindvall@dacsa.se>
2023-10-12 10:50:59 +0200

    json data for trucks and ingredients in public folder

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
-import { trucks } from './trucks';
-import { initialIngredients } from './initalIngredients';
+import  trucks  from './trucks.json';
+import  initialIngredients  from './initialIngredients.json';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
